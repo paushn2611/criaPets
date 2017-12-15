277efbd25f0640bcc78e056421718bcf5e979db7
cristiancanalesdote <31219916+cristiancanalesdote@users.noreply.github.com>
2017-12-15 12:15:48 -0300

    Remove datatable
    se quita datatable de jquery

--- src/app/components/ejemplares/ejemplares.component.ts
+++ src/app/components/ejemplares/ejemplares.component.ts
@@ -14,7 +14,6 @@ export class EjemplaresComponent {
     public rowsOnPage = 10;
     public sortBy = "email";
     public sortOrder = "asc";
-    dtOptions: DataTables.Settings = {};
 
     constructor(
         private http: Http
@@ -23,38 +22,13 @@ export class EjemplaresComponent {
     }
 
     ngOnInit(): void {
-        /* this.http.get("https://jsonplaceholder.typicode.com/posts")
+        this.http.get("https://jsonplaceholder.typicode.com/posts")
             .subscribe((data) => {
                 setTimeout(() => {
                     this.data = data.json();
                     console.log(this.data);
                 }, 1000);
             });
-
-        this.dtOptions = {
-            pagingType: 'full_numbers'
-        }; */
-        this.dtOptions = {
-            ajax: 'data/data.json',
-            columns: [{
-                title: 'ID',
-                data: 'id'
-            }, {
-                title: 'First name',
-                data: 'firstName'
-            }, {
-                title: 'Last name',
-                data: 'lastName'
-            }]
-        };
-    }
-
-    public toInt(num: string) {
-        return +num;
-    }
-
-    public sortByWordLength = (a: any) => {
-        return a.city.length;
     }
 
 }
